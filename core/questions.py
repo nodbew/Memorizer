dd5cc716fb702f67114d42290441703ade96e78a
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-18 19:32:26 +0900

    Update questions.py

--- core/questions.py
+++ core/questions.py
@@ -1,18 +1,29 @@
+import numpy as np
 import streamlit as st
 
-# Initialize variables
-questions:list[str] = []
-answers:list[list[str]] = []
-
-def check_answer(index:int, user_input:str):
+def check_answer(user_input:str):
   """Checks if the user input matches any registered answer"""
-  correct = st.session_state.answers[index]
+  correct = st.session_state.questions[1][st.session_state.index]
   return (user_input in correct)
 
 def add_question(question:str, answers:str) -> None:
   """Adds new questions and its answers"""
-  st.session_state.questions.append(question)
-  st.session_state.answers.append([ans.strip() for ans in answers.split(","))
+  st.session_state.questions = np.concatenate(
+    [
+      st.session_state.questions,
+      np.array(
+        [
+          question,
+          [ans.strip() for ans in answers.split(","),
+          0,
+          0,
+          0,
+        ]
+      ),
+    ],
+    1,
+  )
+      
   return
 
 def delete_question(index:int):
