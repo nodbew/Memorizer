40c2b6a2988a2504bda723477cd406c49e6c3356
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-18 21:51:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import streamlit as st
 
 import core.questions as questions
-import corr.statistics as stats
+import core.statistics as stats
 
 # Initializing session state
 if "questions" not jn st.session_state:
@@ -45,3 +45,24 @@ with main:
             else:
                 st.error(f"不正解...正解は{st.session_state.questions[1][st.session_state.index]}")
             st.session_state.wait = True
+
+with add:
+    st.header("問題追加")
+    question = st.text_input(label = "問題", value = "Hello")
+    st.info("答えは、で区切って入れてください")
+    answers = st.text_input(label = "答え", value = "こんにちは、こんばんは")
+    if st.button("追加"):
+        question.add_auestion(question, answers)
+
+    st.header("問題の削除")
+    target = st.text_input(label = "問題文", value = "削除したい問題を入力...")
+    if st.button("削除"):
+        try:
+            questions.delete_question(st.session_state.questions[0].index(target))
+            st.success("削除しました")
+        except ValueError:
+            st.error("その問題はありません")
+
+with statistics:
+    st.dataframe(stats.get_statistics())
+    if st.button("更新"):pass # For refreshing
