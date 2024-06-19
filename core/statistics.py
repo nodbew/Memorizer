231e966ac12cd13a2358220d584a3b77a74abf4d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-19 10:34:31 +0900

    Update statistics.py

--- core/statistics.py
+++ core/statistics.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-def get_stats():
+def get_statistics():
   # General statistics
   solved = st.session_state.count
   correct = solved - st.session_state.mistakes
