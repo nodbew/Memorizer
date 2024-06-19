870ce25233018ceb83a8a13681b15bcf682d5b1f
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-19 14:05:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,13 @@ if "wait" not in st.session_state:
     st.session_state.wait = False
 
 main, add, statistics = st.tabs(["出題", "問題集", "成績"])
+
 with main:
-    if st.session_state.wait:
-        st.write(st.session_state.questions[0][st.session_state.index])
-        st.write(st.session_state.input)
-        if st.button("次へ"):
-            st.session_state.wait = False
-            st.rerun()
+    if len(st.session_state.questions[0]) == 0:
+        st.error('問題がありません  \n問題を追加してください')
     else:
         st.session_state.index = random.randrange(0, len(st.session_state.questions[0]))
-        st.write(st.session_state.questions[0][st.session_state.index])
+        st.write(st.session_state.questions[0][st.session_state.index]       
         st.session_state.input = st.text_input(label = "答え")
         if st.button("答える"):
             result = questions.check_answer(st.session_state.input)
@@ -46,7 +43,6 @@ with main:
                 st.success("正解！")
             else:
                 st.error(f"不正解...正解は{st.session_state.questions[1][st.session_state.index]}")
-            st.session_state.wait = True
 
 with add:
     st.header("問題追加")
