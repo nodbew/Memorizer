01ccbca20325c3cf15b7204a1ed9c7455f83d853
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-20 12:34:16 +0900

    Update datas.py

--- core/datas.py
+++ core/datas.py
@@ -52,11 +52,14 @@ def from_file(uploaded):
     # Replace overlapping questions' answers with a new concatenated answers list
     questions = np.concatenate([st.session_state.questions, np.array(['\n', [''], 0, 0, 0])], 1) # To absorb all elements that are not overlapping 
     sorter = np.argsort(st.session_state.questions)
-    new_arr_indices = sorter[np.searchsorted(st.session_state.questions, arr, sorter = sorter)]
-    np.where((new_arr_indices == 0 | new_arr_indices == len(st.session_state.questions), -1, new_arr_indices)) # Replace all indices that are not overlapping with -1
+    old_arr_indices = sorter[np.searchsorted(st.session_state.questions, arr, sorter = sorter)]
+    old_arr_indices = np.where((old_arr_indices == 0 | old_arr_indices == len(st.session_state.questions), -1, old_arr_indices)) # Replace all indices that are not overlapping with -1
 
-    
-    
+    # Replace answers
+    old_overlapping_array = st.session_state.questions[1][old_arr_indices]
+    _extend_Lists(arr, old_overlapping_array)
+    del st.session_state.questions[old_arr_indices]
+    del st.session_state.questions[-1] # The array concatenated before
 
     st.session_state.questions = np.concatenate([st.session_state.questions, arr], 1)
 
