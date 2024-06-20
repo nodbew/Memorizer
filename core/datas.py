55e0fdc73c9e26222ef0b262eff53a0ec1ffead9
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-20 10:00:13 +0900

    Update datas.py

--- core/datas.py
+++ core/datas.py
@@ -1,12 +1,38 @@
 import json
+import numpy as np
 from io import StringIO
 
 import numpy as np
 import streamlit as st
 
+from . import questions
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super().default(obj)
+
+class CustomListEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, questions.List):
+            return obj.getlist()
+        else:
+            return super().default(obj)
+
+class CustomEncoder(NumpyArrayEncoder, CustomListEncoder):pass
+
+def custom_List_decode(obj:list) -> questions.List:
+    return questions.List(obj)
+
+def ndarray_decode(obj:list) -> np.ndarray:
+    obj[1] = [custom_List_decode(l) for l in obj[1]]
+    return np.array(obj).reshape((5, -1))
+
 @st.cache_data
 def to_file():
-    return json.dumps(st.session_state.questions.tolist())
+    return json.dumps(st.session_state.questions, cls = CustomEncoder)
 
 def from_file(uploaded):
     '''
@@ -14,7 +40,7 @@ def from_file(uploaded):
     '''
     uploaded = StringIO(uploaded.getvalue().decode('utf-8')).read()
     try:
-        arr = json.loads(uploaded)
+        arr = json.loads(uploaded, object_hook = ndarray_decode)
         arr = np.array(arr).reshape((5, -1))
         
     except JSONDecodeError:
