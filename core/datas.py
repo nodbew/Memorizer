7e4dd66ea0c62647403376b173ff36deab3c0b81
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-20 13:05:13 +0900

    Update datas.py

--- core/datas.py
+++ core/datas.py
@@ -5,7 +5,7 @@ from io import StringIO
 import numpy as np
 import streamlit as st
 
-from . import questions as questions
+from .questions import List
 
 class NumpyArrayEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -26,7 +26,7 @@ class CustomEncoder(NumpyArrayEncoder, CustomListEncoder):pass
 def to_file():
     return json.dumps(st.session_state.questions, cls = CustomEncoder)
 
-def _extend_List_obj(l1:questions.List, l2:questions.List) -> None:
+def _extend_List_obj(l1:List, l2:List) -> None:
     l1.extend(l2)
     return
 _extend_Lists = np.vectorize(_extend_List_obj)
@@ -38,7 +38,7 @@ def from_file(uploaded):
     uploaded:str = StringIO(uploaded.getvalue().decode('utf-8')).read()
     try:
         arr:list = json.loads(uploaded)
-        arr[1]:list = [questions.List(l) for l in arr[1]]
+        arr[1]:list = [List(l) for l in arr[1]]
         arr:np.ndarray = np.array(arr).reshape((5, -1))
         
     except json.JSONDecodeError:
@@ -50,7 +50,7 @@ def from_file(uploaded):
         return
 
     # Replace overlapping questions' answers with a new concatenated answers list
-    questions = np.concatenate([st.session_state.questions, np.array(['\n', questions.List([]), 0, 0, 0])], 1) # To absorb all elements that are not overlapping 
+    questions = np.concatenate([st.session_state.questions, np.array(['\n', List([]), 0, 0, 0])], 1) # To absorb all elements that are not overlapping 
     sorter = np.argsort(st.session_state.questions)
     old_arr_indices = sorter[np.searchsorted(st.session_state.questions, arr, sorter = sorter)]
     old_arr_indices = np.where((old_arr_indices == 0 | old_arr_indices == len(st.session_state.questions), -1, old_arr_indices)) # Replace all indices that are not overlapping with -1
