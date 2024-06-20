146aea5cab9237eeb6c5b04cb72f8d2ad8c7b9e8
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-20 12:23:45 +0900

    Update datas.py

--- core/datas.py
+++ core/datas.py
@@ -26,6 +26,11 @@ class CustomEncoder(NumpyArrayEncoder, CustomListEncoder):pass
 def to_file():
     return json.dumps(st.session_state.questions, cls = CustomEncoder)
 
+def _extend_List_obj(l1:questions.List, l2:questions.List) -> None:
+    l1.extend(l2)
+    return
+_extend_Lists = np.vectorize(_extend_List_obj)
+
 def from_file(uploaded):
     '''
     Takes a st.UploadedFile object, reads it, and adds the data to the questions array.
@@ -44,6 +49,13 @@ def from_file(uploaded):
         st.error('無効なファイル形式です')
         return
 
+    # Replace overlapping questions' answers with a new concatenated answers list
+    questions = np.concatenate([st.session_state.questions, np.array(['\n', [''], 0, 0, 0])], 1) # To absorb all elements that are not overlapping 
+    sorter = np.argsort(st.session_state.questions)
+    new_arr_indices = sorter[np.searchsorted(st.session_state.questions, arr, sorter = sorter)]
+    np.where((new_arr_indices == 0 | new_arr_indices == len(st.session_state.questions), -1, new_arr_indices)) # Replace all indices that are not overlapping with -1
+
+    
     
 
     st.session_state.questions = np.concatenate([st.session_state.questions, arr], 1)
