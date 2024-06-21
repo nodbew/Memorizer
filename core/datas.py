321341ef1b6c2056f686437cf5c781d313c14e7d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-06-21 10:53:24 +0900

    Update datas.py

--- core/datas.py
+++ core/datas.py
@@ -49,16 +49,19 @@ def from_file(uploaded):
         st.error('無効なファイル形式です')
         return
 
-    # Replace overlapping questions' answers with a new concatenated answers list
-    overlapping_old_arr_indices = np.isin(st.session_state.questions[0], arr[0], assume_unique = True)
-    overlapping_new_arr_indices = np.isin(arr[0], st.session_state.questions[0], assume_unique = True)
-    # The arrays should be sorted
-    _extend_Lists(st.session_state.questions[1][overlapping_old_arr_indices], arr[1][overlapping_new_arr_indices])
-    np.delete(arr, overlapping_new_arr_indices.nonzero()) # Delete unneeded column
+    if not len(st.session_state.questions[0]) == 0:
 
-    # Concatenate arrays
-    questions = np.concatenate([st.session_state.questions, arr], 1)
-    st.session_state.questions = questions[np.argsort(questions[0])]
+        # Replace overlapping questions' answers with a new concatenated answers list
+        overlapping_old_arr_indices = np.isin(st.session_state.questions[0], arr[0], assume_unique = True)
+        overlapping_new_arr_indices = np.isin(arr[0], st.session_state.questions[0], assume_unique = True)
+    
+        # The arrays should be sorted ind advance
+        _extend_Lists(st.session_state.questions[1][overlapping_old_arr_indices], arr[1][overlapping_new_arr_indices])
+        np.delete(arr, overlapping_new_arr_indices.nonzero()) # Delete unneeded column
+
+     # Concatenate arrays
+     questions = np.concatenate([st.session_state.questions, arr], 1)
+     st.session_state.questions = questions[np.argsort(questions[0])]
 
     # Update counts
     st.session_state.count += np.sum(arr[2:4])
